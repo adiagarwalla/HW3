a0bd7ca0891f26ffdee04f4c0c98f928db9a4e46
pchang328 <Paul@nat-oitwireless-inside-vapornet100-d-17607.Princeton.EDU>
2015-04-26 10:33:34 -0400

    This python scripts prints the distribution of addresses, both giver and receiver, in the training data file

---
+++ dictRec.py
@@ -0,0 +1,33 @@
+import sys
+import operator
+
+def main(args):
+    fileName = args[0]
+    dataFile = open(fileName, 'rU')
+
+    freq = {}
+    freqGiver = {}
+    length = 444075
+
+    for i in range(length):
+        freq[i] = 0
+        freqGiver[i] = 0
+    
+    for line in dataFile:
+        arr = line.split()
+        freq[int(float(arr[1]))] = freq[int(float(arr[1]))] + 1
+        freqGiver[int(float(arr[0]))] = freqGiver[int(float(arr[0]))] + 1
+
+    sorted_freq = sorted(freq.items(), key=operator.itemgetter(1))
+    sorted_freqGiver = sorted(freqGiver.items(), key=operator.itemgetter(1))
+    #sorted_dict = dict(sorted_freq)
+    for i in range(length, length - 50, -1): 
+        print sorted_freq[i - 1]
+
+    print "----------------------"
+
+    for i in range(length, length - 50, -1): 
+        print sorted_freqGiver[i - 1]
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
